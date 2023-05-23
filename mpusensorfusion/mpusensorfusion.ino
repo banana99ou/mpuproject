1ac348a23684c8ecd10175df936005d0afebae1a
mysmbserver <banana99ou@gmail.com>
2023-05-24 00:07:31 +0900

    wrote something to mpusensorfusion

--- mpusensorfusion/mpusensorfusion.ino
+++ mpusensorfusion/mpusensorfusion.ino
@@ -1,5 +1,19 @@
-void setup() {
-	
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+
+void setup(void) {
+    Serial.begin(115200);
+    if (!mpu.begin()){
+        Serial.println("Failed to find MPU6050);
+        while(1){
+            delay(10);
+        }
+    }
+    Serial.println("Found MPU6050");
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
 }
 
 void loop(){
