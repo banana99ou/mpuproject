1a277b240ad0986757469486272656420e8b84f3
banana <banana99ou@gmail.com>
2023-07-06 07:10:13 +0900

    loosing location esimation

--- mpusensorfusion/mpusensorfusion.ino
+++ mpusensorfusion/mpusensorfusion.ino
@@ -8,7 +8,7 @@ Adafruit_MPU6050 mpu;
 unsigned long now, before;
 unsigned long dt;
 float dt_seconds;
-float alpha;
+float alpha = 0.0;
 
 float acceleration[3] = {0, 0, 0};
 float angular_acceleration[3] = {0, 0, 0};
@@ -30,70 +30,140 @@ float offset_gyro_x, offset_gyro_y, offset_gyro_z = 0;
 
 int NofInitTest = 500;
 
-// Function to rotate the acceleration vector
-void adjust_acceleration_offset(float& offset_accel_x, float& offset_accel_y, float& offset_accel_z, float attitude[3]) {
-  // Extract the rotation angles from the attitude array
-  float a = attitude[0]; // Rotation around x-axis
-  float b = attitude[1]; // Rotation around y-axis
-  float c = attitude[2]; // Rotation around z-axis
-
-  // Rotation matrix around the x-axis
-  float x_rot[3][3] = {
-    {1, 0, 0},
-    {0, cos(a), -sin(a)},
-    {0, sin(a), cos(a)}
-  };
-
-  // Rotation matrix around the y-axis
-  float y_rot[3][3] = {
-    {cos(b), 0, sin(b)},
-    {0, 1, 0},
-    {-sin(b), 0, cos(b)}
-  };
-
-  // Rotation matrix around the z-axis
-  float z_rot[3][3] = {
-    {cos(c), -sin(c), 0},
-    {sin(c), cos(c), 0},
-    {0, 0, 1}
-  };
-
-  // Perform the rotations
-  float result[3] = {offset_accel_x, offset_accel_y, offset_accel_z};
-
-  // Rotate around the z-axis
-  float temp[3];
-  for (int i = 0; i < 3; i++) {
-    temp[i] = 0;
-    for (int j = 0; j < 3; j++) {
-      temp[i] += z_rot[i][j] * result[j];
-    }
-  }
-
-  // Rotate around the y-axis
-  for (int i = 0; i < 3; i++) {
-    result[i] = 0;
-    for (int j = 0; j < 3; j++) {
-      result[i] += y_rot[i][j] * temp[j];
-    }
-  }
-
-  // Rotate around the x-axis
-  for (int i = 0; i < 3; i++) {
-    temp[i] = 0;
-    for (int j = 0; j < 3; j++) {
-      temp[i] += x_rot[i][j] * result[j];
-    }
-  }
-
-  // Update the output variables
-  offset_accel_x = temp[0];
-  offset_accel_y = temp[1];
-  offset_accel_z = temp[2];
-}
-
 void setup(void) {
   Serial.begin(115200);
+  mpu__init__();
+
+  Serial.println("");
+  delay(100);
+
+  //Use a loop to take 100 readings and sum the values for each axis
+  for (int i = 0; i < NofInitTest; i++) {
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+    raw_accel_x = a.acceleration.x;
+    raw_accel_y = a.acceleration.y;
+    raw_accel_z = a.acceleration.z;
+    raw_gyro_x = g.gyro.x;
+    raw_gyro_y = g.gyro.y;
+    raw_gyro_z = g.gyro.z;
+
+    sum_accel_x += raw_accel_x;
+    sum_accel_y += raw_accel_y;
+    sum_accel_z += raw_accel_z;
+    sum_gyro_x += raw_gyro_x;
+    sum_gyro_y += raw_gyro_y;
+    sum_gyro_z += raw_gyro_z;
+
+    delay(1);
+  }
+
+  offset_accel_x = -(sum_accel_x / NofInitTest);
+  offset_accel_y = -(sum_accel_y / NofInitTest);
+  offset_accel_z = -(sum_accel_z / NofInitTest);
+  offset_gyro_x = -(sum_gyro_x / NofInitTest);
+  offset_gyro_y = -(sum_gyro_y / NofInitTest);
+  offset_gyro_z = -(sum_gyro_z / NofInitTest);
+
+  //ignore init time
+  now = millis();
+  dt = (now - before);
+  dt_seconds = (float) dt / 1000.0;
+  before = now;
+}
+
+void loop() {
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  if(now/10 > NofInitTest*2) {
+    //attitude by gravity
+    accelgyro[0] = atan((acceleration[1]-offset_accel_y) / sqrt(acceleration[0]*acceleration[0]+acceleration[2]*acceleration[2]));
+    accelgyro[1] = atan((acceleration[0]-offset_accel_x) / sqrt(acceleration[1]*acceleration[1]+acceleration[2]*acceleration[2]));
+    accelgyro[2] = atan(sqrt(acceleration[0]*acceleration[0]+acceleration[2]*acceleration[2]) / (acceleration[2]-offset_accel_z));
+
+    angular_acceleration[0] = g.gyro.x + offset_gyro_x;
+    angular_acceleration[1] = g.gyro.y + offset_gyro_y;
+    angular_acceleration[2] = g.gyro.z + offset_gyro_z;
+
+    angular_velocity[0] += angular_acceleration[0] * dt_seconds;
+    angular_velocity[1] += angular_acceleration[1] * dt_seconds;
+    angular_velocity[2] += angular_acceleration[2] * dt_seconds;
+
+    attitude[0] += alpha * (angular_velocity[0] * dt_seconds) + (1 - alpha) * (accelgyro[0]);
+    attitude[1] += alpha * (angular_velocity[1] * dt_seconds) + (1 - alpha) * (accelgyro[1]);
+    attitude[2] += alpha * (angular_velocity[2] * dt_seconds) + (1 - alpha) * (accelgyro[2]);
+  }
+  //calculate delta T
+  now = millis();
+  dt = (now - before);
+  dt_seconds = (float) dt / 1000.0;
+  before = now;
+
+  // Serial.print(dt_seconds);
+  // Serial.print(", ");
+  // Serial.print(a.acceleration.x);
+  // Serial.print(", ");
+  // Serial.print(a.acceleration.y);
+  // Serial.print(", ");
+  // Serial.print(a.acceleration.z);
+  // Serial.print(", ");
+  // Serial.print(offset_accel_x);
+  // Serial.print(", ");
+  // Serial.print(offset_accel_y);
+  // Serial.print(", ");
+  // Serial.print(offset_accel_z);
+  // Serial.print(", ");
+  // Serial.print(acceleration[0]);
+  // Serial.print(", ");
+  // Serial.print(acceleration[1]);
+  // Serial.print(", ");
+  // Serial.print(acceleration[2]);
+  // Serial.print(", ");
+  // Serial.print(velocity[0]);
+  // Serial.print(", ");
+  // Serial.print(velocity[1]);
+  // Serial.print(", ");
+  // Serial.print(velocity[2]);
+  // Serial.print(", ");
+  // Serial.println(location[0]);
+  // Serial.print(", ");
+  // Serial.print(location[1]);
+  // Serial.print(", ");
+  // Serial.print(location[2]);
+  // Serial.print(", ");
+  // Serial.print(g.gyro.x);
+  // Serial.print(", ");
+  // Serial.print(g.gyro.y);
+  // Serial.print(", ");
+  // Serial.print(g.gyro.z);
+  // Serial.print(", ");
+  // Serial.print(offset_gyro_x);
+  // Serial.print(", ");
+  // Serial.print(offset_gyro_y);
+  // Serial.print(", ");
+  // Serial.print(offset_gyro_z);
+  // Serial.print(", ");
+  Serial.print(angular_acceleration[0]);
+  Serial.print(", ");
+  // Seiral.print(angular_acceleration[1]);
+  // Serial.print(", ");
+  // Serial.print(angular_acceleration[2]);
+  // Serial.print(", ");
+  Serial.print(angular_velocity[0]);
+  Serial.print(", ");
+  // Serial.print(angular_velocity[1]);
+  // Serial.print(", ");
+  // Serial.print(angular_velocity[2]);
+  // Serial.print(", ");
+  Serial.println(attitude[0]);
+  // Serial.print(", ");
+  // Serial.print(attitude[1]);
+  // Serial.print(", ");
+  // Serial.println(attitude[2]);
+}
+
+void mpu__init__() {
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050");
     while (1) {
@@ -159,143 +229,4 @@ void setup(void) {
       Serial.println("5 Hz");
       break;
   }
-
-  Serial.println("");
-  delay(100);
-
-  //Use a loop to take 100 readings and sum the values for each axis
-  for (int i = 0; i < NofInitTest; i++) {
-    sensors_event_t a, g, temp;
-    mpu.getEvent(&a, &g, &temp);
-    raw_accel_x = a.acceleration.x;
-    raw_accel_y = a.acceleration.y;
-    raw_accel_z = a.acceleration.z;
-    raw_gyro_x = g.gyro.x;
-    raw_gyro_y = g.gyro.y;
-    raw_gyro_z = g.gyro.z;
-
-    sum_accel_x += raw_accel_x;
-    sum_accel_y += raw_accel_y;
-    sum_accel_z += raw_accel_z;
-    sum_gyro_x += raw_gyro_x;
-    sum_gyro_y += raw_gyro_y;
-    sum_gyro_z += raw_gyro_z;
-
-    delay(1);
-  }
-
-  offset_accel_x = -(sum_accel_x / NofInitTest);
-  offset_accel_y = -(sum_accel_y / NofInitTest);
-  offset_accel_z = -(sum_accel_z / NofInitTest);
-  offset_gyro_x = -(sum_gyro_x / NofInitTest);
-  offset_gyro_y = -(sum_gyro_y / NofInitTest);
-  offset_gyro_z = -(sum_gyro_z / NofInitTest);
-
-  //ignore init time
-  now = millis();
-  dt = (now - before);
-  dt_seconds = (float) dt / 1000.0;
-  before = now;
-}
-
-void loop() {
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
-
-  if(now/10 > NofInitTest*2) {
-    //attitude by gravity
-    accelgyro[0] = atan((acceleration[1]-offset_accel_y) / sqrt(acceleration[0]*acceleration[0]+acceleration[2]*acceleration[2]));
-    accelgyro[1] = atan((acceleration[0]-offset_accel_x) / sqrt(acceleration[1]*acceleration[1]+acceleration[2]*acceleration[2]));
-    accelgyro[2] = atan(sqrt(acceleration[0]*acceleration[0]+acceleration[2]*acceleration[2]) / (acceleration[2]-offset_accel_z));
-
-    angular_acceleration[0] = g.gyro.x + offset_gyro_x;
-    angular_acceleration[1] = g.gyro.y + offset_gyro_y;
-    angular_acceleration[2] = g.gyro.z + offset_gyro_z;
-
-    angular_velocity[0] += angular_acceleration[0] * dt_seconds;
-    angular_velocity[1] += angular_acceleration[1] * dt_seconds;
-    angular_velocity[2] += angular_acceleration[2] * dt_seconds;
-
-    attitude[0] += alpha * (angular_velocity[0] * dt_seconds) + (1 - alpha) * (accelgyro[0]);
-    attitude[1] += alpha * (angular_velocity[1] * dt_seconds) + (1 - alpha) * (accelgyro[1]);
-    attitude[2] += alpha * (angular_velocity[2] * dt_seconds) + (1 - alpha) * (accelgyro[2]);
-
-    //adjust acceleration offset
-    //adjust_acceleration_offset(offset_accel_x, offset_accel_y, offset_accel_z, attitude);
-
-    velocity[0] += acceleration[0] * dt_seconds;
-    velocity[1] += acceleration[1] * dt_seconds;
-    velocity[2] += acceleration[2] * dt_seconds;
-
-    location[0] += velocity[0] * dt_seconds;
-    location[1] += velocity[1] * dt_seconds;
-    location[2] += velocity[2] * dt_seconds;
-  }
-  //calculate delta T
-  now = millis();
-  dt = (now - before);
-  dt_seconds = (float) dt / 1000.0;
-  before = now;
-
-  // Serial.print(dt_seconds);
-  // Serial.print(", ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", ");
-  // Serial.print(a.acceleration.y);
-  // Serial.print(", ");
-  // Serial.print(a.acceleration.z);
-  // Serial.print(", ");
-  // Serial.print(offset_accel_x);
-  // Serial.print(", ");
-  // Serial.print(offset_accel_y);
-  // Serial.print(", ");
-  // Serial.print(offset_accel_z);
-  // Serial.print(", ");
-  Serial.print(acceleration[0]);
-  Serial.print(", ");
-  // Serial.print(acceleration[1]);
-  // Serial.print(", ");
-  // Serial.print(acceleration[2]);
-  // Serial.print(", ");
-  Serial.print(velocity[0]);
-  Serial.print(", ");
-  // Serial.print(velocity[1]);
-  // Serial.print(", ");
-  // Serial.print(velocity[2]);
-  // Serial.print(", ");
-  Serial.println(location[0]);
-  // Serial.print(", ");
-  // Serial.print(location[1]);
-  // Serial.print(", ");
-  // Serial.print(location[2]);
-  // Serial.print(", ");
-  // Serial.print(g.gyro.x);
-  // Serial.print(", ");
-  // Serial.print(g.gyro.y);
-  // Serial.print(", ");
-  // Serial.print(g.gyro.z);
-  // Serial.print(", ");
-  // Serial.print(offset_gyro_x);
-  // Serial.print(", ");
-  // Serial.print(offset_gyro_y);
-  // Serial.print(", ");
-  // Serial.print(offset_gyro_z);
-  // Serial.print(", ");
-  // Seiral.print(angular_acceleration[0]);
-  // Serial.print(", ");
-  // Seiral.print(angular_acceleration[1]);
-  // Serial.print(", ");
-  // Seiral.print(angular_acceleration[2]);
-  // Serial.print(", ");
-  // Serial.print(angular_velocity[0]);
-  // Serial.print(", ");
-  // Serial.print(angular_velocity[1]);
-  // Serial.print(", ");
-  // Serial.print(angular_velocity[2]);
-  // Serial.print(", ");
-  // Serial.print(attitude[0]);
-  // Serial.print(", ");
-  // Serial.print(attitude[1]);
-  // Serial.print(", ");
-  // Serial.println(attitude[2]);
 }
